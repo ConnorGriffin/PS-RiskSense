f8d811c18ce98affc6a9c162380dad9ccfce8d34
Connor Griffin <168191@az.int>
2020-02-12 17:21:11 -0700

    Add rudimentary filtering to some export functions

--- PS-RiskSense/Public/Invoke-RiskSenseFindingExport.ps1
+++ PS-RiskSense/Public/Invoke-RiskSenseFindingExport.ps1
@@ -14,6 +14,18 @@ function Invoke-RiskSenseFindingExport {
         # ClientID
         [Parameter(Mandatory)]
         [int]$ClientID,
+
+        # Filter on "Status"
+        [ValidateSet('Open', 'Closed')]
+        [string]$Status,
+
+        # Filter on "Has Patch"
+        [AllowNull()]
+        [nullable[bool]]$HasPatch,
+
+        # Filter on "Has Privilege Escalation or Remote Code Execution Exploits"
+        [AllowNull()]
+        [nullable[bool]]$HasPeRce,
         
         # RiskSense API Key
         [Parameter(Mandatory)]
@@ -22,22 +34,49 @@ function Invoke-RiskSenseFindingExport {
 
     begin {
         $headers = Get-AuthHeader $Token
-        $body = '{
-          "filterRequest": {
-            "filters": [
-                {"field":"generic_state","exclusive":false,"operator":"EXACT","value":"Open"}
-            ]
-          },
-          "fileType": "XLSX"
-        }'
+        $filters = @()
+
+        if ($Status) {
+            $filters += [PSCustomObject]@{
+                field = 'generic_state'
+                exclusive = $false
+                operator = 'EXACT'
+                value = $Status
+            }
+        }
+
+        if ($HasPeRce -ne $null) {
+            $filters += [PSCustomObject]@{
+                field = 'has_pe_rce'
+                exclusive = $false
+                operator = 'EXACT'
+                value = ([string]$HasPeRce).ToLower()
+            }
+        }
+
+        if ($HasPatch -ne $null) {
+            $filters += [PSCustomObject]@{
+                field = 'has_patch'
+                exclusive = $false
+                operator = 'EXACT'
+                value = ([string]$HasPatch).ToLower()
+            }
+        }
+
+        $body = [PSCustomObject]@{
+            filterRequest = [PSCustomObject]@{
+                filters = $filters 
+            }
+            fileType = 'XLSX'
+        } | ConvertTo-Json -Depth 99
     }
 
     process {
-         $result = Invoke-RestMethod -Uri "$uri/client/$ClientID/hostFinding/export" -Method Post -Body $body -Headers $headers
-         [PSCustomObject]@{
-             ID = $result.id
-             Created = Get-Date $result.created
-         }
+        $result = Invoke-RestMethod -Uri "$uri/client/$ClientID/hostFinding/export" -Method Post -Body $body -Headers $headers
+        [PSCustomObject]@{
+            ID = $result.id
+            Created = Get-Date $result.created
+        }
     }
 
     end {}
